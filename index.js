bbd58f3bbf38182dfc3a8f4ef4127845b6cee1f3
basu-shambo <54249103+basu-shambo@users.noreply.github.com>
2021-09-05 04:52:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const root= document.querySelector(".root");
 const das = document.querySelector("body > div > button.button.das");
 const basu = document.querySelector("body > div > button.button.basu");
-const coins = document.querySelectorAll(".coin")
+const coins = document.querySelectorAll(".coin");
+const dasIndicator = document.querySelector("body > div > button.button.das > span");
+const basuIndicator = document.querySelector("body > div > button.button.basu >span")
+
 das.addEventListener('click',()=>{
     coins.forEach((coin)=>{
         coin.classList.remove("basu");
         coin.classList.add("das");
         coin.classList.toggle("red");
         coin.classList.toggle("yellow");
+        dasIndicator.innerText = "\uD83D\uDD34"
+        basuIndicator.innerText= "🟢"
         const i=Math.floor(2*Math.random());
         coin.innerText = i;
     })
@@ -18,6 +23,8 @@ basu.addEventListener('click',()=>{
         coin.classList.add("basu");
         coin.classList.toggle("red");
         coin.classList.toggle("yellow");
+        basuIndicator.innerText = "\uD83D\uDD34";
+        dasIndicator.innerText= "🟢";
         const i=Math.floor(2*Math.random());
         coin.innerText = i;
     })
