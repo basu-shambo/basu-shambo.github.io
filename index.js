5c8a8216d009912350cad967019d55cc4dfe4326
basu-shambo <54249103+basu-shambo@users.noreply.github.com>
2021-09-11 12:11:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const das = document.querySelector("body > div > button.button.das");
 const basu = document.querySelector("body > div > button.button.basu");
 const coins = document.querySelectorAll(".coin");
 const dasIndicator = document.querySelector("body > div > button.button.das > span");
-const basuIndicator = document.querySelector("body > div > button.button.basu >span")
-
+const basuIndicator = document.querySelector("body > div > button.button.basu >span");
+const statsButton = document.querySelector("body > div > button.stats");
+const dataViewer = document.querySelector(".data");
+const close  = document.querySelector("#close");
+const graph = document.querySelector(".graph");
 das.addEventListener('click',()=>{
+    let total =0 ;
     coins.forEach((coin)=>{
         coin.classList.remove("basu");
         coin.classList.add("das");
@@ -15,9 +19,18 @@ das.addEventListener('click',()=>{
         basuIndicator.innerText= "🟢"
         const i=Math.floor(2*Math.random());
         coin.innerText = i;
+        total+=i;
     })
+    let a = {};
+    if(localStorage.getItem('data') != null){
+        a = JSON.parse(localStorage.getItem('data'));
+    } 
+    a[total] = a[total]?a[total]+1:1
+    localStorage.setItem('data',JSON.stringify(a));
+    updateStats();
 })
 basu.addEventListener('click',()=>{
+    let total =0 ;
     coins.forEach((coin)=>{
         coin.classList.remove("das");
         coin.classList.add("basu");
@@ -27,5 +40,29 @@ basu.addEventListener('click',()=>{
         dasIndicator.innerText= "🟢";
         const i=Math.floor(2*Math.random());
         coin.innerText = i;
+        total+=i;
     })
+    let a = {};
+    if(localStorage.getItem('data') != null){
+        a = JSON.parse(localStorage.getItem('data'));
+    } 
+    a[total] = a[total]?a[total]+1:1
+    localStorage.setItem('data',JSON.stringify(a));
+    updateStats();
 })
+
+statsButton.addEventListener('click',(e)=>{
+    dataViewer.style.display = "block"
+    updateStats();
+})
+close.addEventListener('click',()=>{
+    dataViewer.style.display = "none";
+})
+
+const updateStats = () => {
+    const pre = document.createElement('pre');
+    pre.innerText = localStorage.getItem('data');
+    graph.innerHTML = '';
+    graph.appendChild(pre);
+
+}
